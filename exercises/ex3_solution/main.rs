f84de2fdd9c3bb2fd71be86224a3d6002abf8184
N0elurt <brynildsenwalter@gmail.com>
2025-01-28 17:47:32 +0100

    La til oppgave å jobbe videre på

---
+++ exercises/ex3_solution/main.rs
@@ -0,0 +1,47 @@
+use std::thread::*;
+use std::time::*;
+
+use crossbeam_channel as cbc;
+
+use driver_rust::elevio;
+use driver_rust::elevio::elev as e;
+
+fn main() -> std::io::Result<()> {
+    let elev_num_floors = 4;
+    let elevator = e::Elevator::init("localhost:15657", elev_num_floors)?;
+    println!("Elevator started:\n{:#?}", elevator);
+
+    let poll_period = Duration::from_millis(25);
+
+    let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>();
+    {
+        let elevator = elevator.clone();
+        spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
+    }
+
+    let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
+    {
+        let elevator = elevator.clone();
+        spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
+    }
+
+    let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
+    {
+        let elevator = elevator.clone();
+        spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
+    }
+
+    let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
+    {
+        let elevator = elevator.clone();
+        spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
+    }
+
+    let mut dirn = e::DIRN_DOWN;
+    if elevator.floor_sensor().is_none() {
+        elevator.motor_direction(dirn);
+    }
+
+
+    
+}
