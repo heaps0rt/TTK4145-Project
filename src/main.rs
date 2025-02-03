14e9281d24456093b46ff2a223238ee5b1851b0f
N0elurt <brynildsenwalter@gmail.com>
2025-02-03 22:38:20 +0100

    Delt opp heis og master i threads

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use driver_rust::elevio::elev::DIRN_UP;
 use driver_rust::elevio::elev::HALL_DOWN;
 use driver_rust::elevio::elev::HALL_UP;
 use driver_rust::elevio::elev as e;
+use driver_rust::elevio::poll;
 use driver_rust::elevio::poll::CallButton;
 
 pub const WAITING: u8 = 0;
@@ -163,17 +164,10 @@ fn check_lights(elevator: &Elevator, dirn: u8, floor: u8, num_floors: u8) -> () 
     }
 }
 
-fn main() -> std::io::Result<()> {
+fn runMaster(elev_num_floors: u8, elevator: Elevator, poll_period: Duration) -> () {
+
     // Setting up durations for later use
     let a_hundred_millis = Duration::from_millis(100);
-    let five_hundred_millis = Duration::from_millis(500);
-    let now = Instant::now();
-
-    let elev_num_floors = 4; // Set floor count
-    let elevator = e::Elevator::init("localhost:15657", elev_num_floors)?; // Initialize and connect elevator
-    println!("Elevator started:\n{:#?}", elevator);
-
-    let poll_period = Duration::from_millis(25); // Set poll period for buttons and sensors
 
     let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>(); // Initialize call buttons
     {
@@ -181,26 +175,83 @@ fn main() -> std::io::Result<()> {
         spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
     }
 
+    // Setting up set with Rwlock
+    // Rwlock means that it can either be written to by a single thread or read by any number of threads at once
+    let mut order_list = RwLock::from(HashSet::new());
+    let mut status_list = RwLock::from(HashSet::from([0]));
+    let mut elevator_direction = e::DIRN_STOP;
+
+    loop {
+        cbc::select! {
+            recv(call_button_rx) -> a => { // Get info from call button and add it to the list of floors ordered if it is a hall call
+                let call_button = a.unwrap();
+                if call_button.floor == e::HALL_DOWN | HALL_UP {
+                    let new_order = Order {
+                        floor_number: call_button.floor,
+                        direction: call_button.call,
+                        status: WAITING,
+                    };
+                    let mut order_list_w = order_list.write().unwrap();
+                    order_list_w.insert(new_order);
+                    elevator.call_button_light(call_button.floor, call_button.call, true);
+
+                }
+        
+            }
+            // This function polls continuously
+            default(a_hundred_millis) => {
+                // Status update readout
+                let status_list_w = status_list.write().unwrap();
+                println!("-------Status---------");
+                println!("Destinasjoner: {:#?}", status_list_w);
+                match elevator_direction{
+                    e::DIRN_DOWN => {
+                        println!("Retning: Ned");
+                    }
+                    e::DIRN_UP => {
+                        println!("Retning: Opp");
+                    }
+                    e::DIRN_STOP => {
+                        println!("Retning: Stoppet");
+                    }
+                    2_u8..=254_u8 => {
+                        println!("Noe har gått kraftig galt");
+                    }
+                }
+                println!("-------Slutt----------");
+            }
+        }
+    }
+}
+
+fn runElevator(elev_num_floors: u8, elevator: Elevator, poll_period: Duration) -> () {
+
+    // Setting up durations for later use
+    let a_hundred_millis = Duration::from_millis(100);
+    let five_hundred_millis = Duration::from_millis(500);
+    let now = Instant::now();
+
+    let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>(); // Initialize call buttons
+    {
+        let elevator = elevator.clone();
+        spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
+    }
     let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>(); // Initialize floor sensor
     {
         let elevator = elevator.clone();
         spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
     }
-
     let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>(); // Initialize stop button
     {
         let elevator = elevator.clone();
         spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
     }
-
     let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>(); // Initialize obstruction switch
     {
         let elevator = elevator.clone();
         spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
     }
 
-
-    
     let mut dirn = e::DIRN_DOWN; // Set mutable direction
     // If the elevator is not on a floor, send it down
     elevator.motor_direction(dirn);
@@ -208,129 +259,133 @@ fn main() -> std::io::Result<()> {
     
 
     let mut last_floor: u8 = elev_num_floors+1;
-
-    // Setting up sets with Rwlock
+    // Setting up set with Rwlock
     // Rwlock means that it can either be written to by a single thread or read by any number of threads at once
-    let mut order_list = RwLock::from(HashSet::new());
     let mut destination_list = RwLock::from(HashSet::from([0]));
-
-    spawn(move ||
-        loop {
-            cbc::select! {
-                // This only starts when button press is registered
-                recv(call_button_rx) -> a => { // Get info from call button and add it to the list of floors ordered
-                    let call_button = a.unwrap();
-                    //println!("{:#?}", call_button);
-                    let new_order = Order {
-                        floor_number: call_button.floor,
-                        direction: call_button.call,
-                        status: WAITING,
-                    };
-                    if new_order.direction == e::CAB && new_order.floor_number != last_floor {
-                        let mut order_list_w = order_list.write().unwrap();
-                        order_list_w.insert(new_order);
-                        elevator.call_button_light(call_button.floor, call_button.call, true);
-                    }
-                    else if new_order.direction == e::HALL_UP || new_order.direction == e::HALL_DOWN {
-                        let mut order_list_w = order_list.write().unwrap();
-                        order_list_w.insert(new_order);
-                        elevator.call_button_light(call_button.floor, call_button.call, true);
-                    }
-                    
-                    //print_order(&new_order);
-                
-                }
-            
-                // This only starts when new floor is detected
-                recv(floor_sensor_rx) -> a => { // Get floor status and save last floor for later use
-                    let floor = a.unwrap();
-                    //println!("Floor: {:#?}", floor);
-                    last_floor = floor;
-                    //println!("Last floor updated to: {:#?}", last_floor);
-                    check_lights(&elevator, dirn, floor, elev_num_floors);
-
-                    let mut destination_list_w = destination_list.write().unwrap();
-                    
-                    if destination_list_w.contains(&floor){
-                        elevator.motor_direction(e::DIRN_STOP);
-                        println!("Stopper midlertidig");
-                        destination_list_w.remove(&floor);
-                        elevator.door_light(true);
-                        sleep(five_hundred_millis);
-                        assert!(now.elapsed() >= five_hundred_millis);
-                        elevator.door_light(false);
-                        if destination_list_w.is_empty(){
-                            dirn = e::DIRN_STOP;
-                        }
-                        elevator.motor_direction(dirn);
-                        println!("Fortsetter");
-                        
-                    }
-                    if dirn == e::DIRN_UP && floor == (elev_num_floors-1) {
-                        dirn = e::DIRN_STOP;
-                        elevator.motor_direction(dirn);
-                        if !destination_list_w.is_empty() {
-                            dirn = e::DIRN_DOWN;
-                            elevator.motor_direction(dirn);
-                        }
-                    }
-                    else if dirn == e::DIRN_DOWN && floor == 0 {
-                        dirn = e::DIRN_STOP;
-                        elevator.motor_direction(dirn);
-                        if !destination_list_w.is_empty() {
-                            dirn = e::DIRN_UP;
-                            elevator.motor_direction(dirn);
-                        }
-                    }    
-                }
-                // This function polls continuously
-                default(a_hundred_millis) => {
-                    //  Unlocking Rw locked lists
-                    let mut order_list_w = order_list.write().unwrap();
-                    let mut destination_list_w = destination_list.write().unwrap();
-
-                    // Calling order scheduler
-                    let out = commute_orders(&order_list_w, &destination_list_w, last_floor, &dirn, &elevator, elev_num_floors);
-
-                    // Overwriting old variables with new modified ones
-                    *order_list_w = out.0;
-                    *destination_list_w = out.1;
-                    dirn = out.2;
-
-                    // Status update readout
-                    println!("-------Status---------");
-                    println!("Destinasjoner: {:#?}", destination_list_w);
-                    match dirn{
+    loop {
+        cbc::select! {
+            recv(call_button_rx) -> a => { // Get info from call button and add it to the destination list if it is a cab call
+                let call_button = a.unwrap();
+                if call_button.floor == e::CAB {
+                    match dirn {
                         e::DIRN_DOWN => {
-                            println!("Retning: Ned");
+                            if call_button.floor < last_floor {
+                                let mut destination_list_w = destination_list.write().unwrap();
+                                destination_list_w.insert(call_button.floor);
+                            }
                         }
                         e::DIRN_UP => {
-                            println!("Retning: Opp");
+                            if call_button.floor > last_floor {
+                                let mut destination_list_w = destination_list.write().unwrap();
+                                destination_list_w.insert(call_button.floor);
+                            }
                         }
                         e::DIRN_STOP => {
-                            println!("Retning: Stoppet");
+                            let mut destination_list_w = destination_list.write().unwrap();
+                            destination_list_w.insert(call_button.floor);
                         }
                         2_u8..=254_u8 => {
-                            println!("Noe har gått kraftig galt");
+                            println!("Error in cab order")
                         }
                     }
-                    println!("-------Slutt----------");
+                    
                 }
-                
             }
-        
-    });
+            recv(floor_sensor_rx) -> a => { // Get floor status and save last floor for later use
+                let floor = a.unwrap();
+                //println!("Floor: {:#?}", floor);
+                last_floor = floor;
+                //println!("Last floor updated to: {:#?}", last_floor);
+                check_lights(&elevator, dirn, floor, elev_num_floors);
 
-    // Elevator functions
-    spawn(move || {
-        loop {
-            cbc::select! {
-                default(a_hundred_millis) => {
-                    println!("WTF")
+                let mut destination_list_w = destination_list.write().unwrap();
+                
+                if destination_list_w.contains(&floor){
+                    elevator.motor_direction(e::DIRN_STOP);
+                    println!("Stopper midlertidig");
+                    destination_list_w.remove(&floor);
+                    elevator.door_light(true);
+                    sleep(five_hundred_millis);
+                    assert!(now.elapsed() >= five_hundred_millis);
+                    elevator.door_light(false);
+                    if destination_list_w.is_empty(){
+                        dirn = e::DIRN_STOP;
+                    }
+                    elevator.motor_direction(dirn);
+                    println!("Fortsetter");
+                    
                 }
+                if dirn == e::DIRN_UP && floor == (elev_num_floors-1) {
+                    dirn = e::DIRN_STOP;
+                    elevator.motor_direction(dirn);
+                    if !destination_list_w.is_empty() {
+                        dirn = e::DIRN_DOWN;
+                        elevator.motor_direction(dirn);
+                    }
+                }
+                else if dirn == e::DIRN_DOWN && floor == 0 {
+                    dirn = e::DIRN_STOP;
+                    elevator.motor_direction(dirn);
+                    if !destination_list_w.is_empty() {
+                        dirn = e::DIRN_UP;
+                        elevator.motor_direction(dirn);
+                    }
+                }
+            }
+            // This function polls continuously
+            default(a_hundred_millis) => {
+
             }
         }
-    });
-    Ok(())
+    }
+}
 
+fn main() -> std::io::Result<()> {
+    
+
+    let elev_num_floors = 4; // Set floor count
+    let elevator = e::Elevator::init("localhost:15657", elev_num_floors)?; // Initialize and connect elevator
+    println!("Elevator started:\n{:#?}", elevator);
+
+    let poll_period = Duration::from_millis(25); // Set poll period for buttons and sensors
+    {
+    let elevator = elevator.clone();
+    let master = spawn(move || {
+        runMaster(elev_num_floors, elevator, poll_period);
+    });
+    }
+
+    {
+    let elevator = elevator.clone();
+    let elevator1 = spawn(move || {
+        runElevator(elev_num_floors, elevator, poll_period);
+    });
+    }  
+    
+
+    /* loop {
+        cbc::select! {
+            // This function polls continuously
+            default(a_hundred_millis) => {
+                //  Unlocking Rw locked lists
+                let mut order_list_w = order_list.write().unwrap();
+                let mut destination_list_w = destination_list.write().unwrap();
+
+                // Calling order scheduler
+                let out = commute_orders(&order_list_w, &destination_list_w, last_floor, &dirn, &elevator, elev_num_floors);
+
+                // Overwriting old variables with new modified ones
+                *order_list_w = out.0;
+                *destination_list_w = out.1;
+                dirn = out.2;
+
+                
+            }
+            
+        }
+
+    } */
+   loop {
+
+   }
 }
