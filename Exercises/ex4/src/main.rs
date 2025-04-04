f841e04d4efc2ef53ac5d0dbc140a5250e7dec69
Mortenxy <morten.alteras@gmail.com>
2025-04-04 16:46:59 +0200

    Working process pairs counter

--- Exercises/ex4/src/main.rs
+++ Exercises/ex4/src/main.rs
@@ -9,37 +9,58 @@ use std::io::{BufRead, BufReader, Seek, SeekFrom};
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
-
-pub const COUNT: &str = "count.txt";
+use std::thread::sleep;
+use std::time::Instant;
 
 fn master_alive() -> u8 {
-    let file = File::open(COUNT).unwrap();
+    let file = File::open("count.txt").unwrap();
     let mut last_line = String::new();
-    loop {
+    let mut update_time = Instant::now();
+
+    while update_time.elapsed() <= Duration::from_millis(1500) {
         let reader = BufReader::new(&file);
         if let Some(Ok(line)) = reader.lines().last() {
             if line != last_line {
                 last_line = line;
+                update_time = Instant::now();
             }
         }
+        sleep(Duration::from_millis(100));
     }
 
-    return 8;
+    return last_line.parse().unwrap();
 }
 
-fn counter(){
-    let time = Local::now().to_rfc3339();
+fn counter(mut count: u8){
+    let mut file = OpenOptions::new().append(true).open("count.txt").unwrap();
+    loop {
+        count += 1;
+        writeln!(file, "{}", count).unwrap();
+        println!("{}",count);
+        sleep(Duration::from_millis(1000));
+    }
 }
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args[1]!="backup" {File::create("count.txt")?;}
 
+    // Initialize file if process is not a backup
+    let args: Vec<String> = env::args().collect();
+    if (args.len() == 1) || (args[1] != "backup") {
+        let mut file = File::create("count.txt")?;
+        writeln!(file, "{}", 0).unwrap();
+    }
+
+    // Poll master until it is dead, return the state
     let count = master_alive();
 
-    let mut file = OpenOptions::new().append(true).open("count.txt")?;
-    file.write_all(b"\nAppending this line")?;
-    file.write_all(b"\nAppending this other line")?;
-    
+    // Spawn a backup
+    Command::new("cmd")
+    .args(&["/C", "start", "cmd", "/K", "target\\debug\\ex4.exe", "backup"])
+    .spawn()
+    .unwrap();
+
+    // Continue counting from the previous state
+    counter(count);
+
     Ok(())
 }
